4c000726cb859551c32ccdbf92343a226ab52d1e
isabellekeutner <51681475+isabellekeutner@users.noreply.github.com>
2019-07-04 22:58:12 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
          model = Post
          fields = ('title', 'text', 'categorie',)
+
+    class Meta:
+         model = Event
+         fields = ('title', 'text',)
