ceef4d3b558340c97b9bef421173f14e76e6723e
isabellekeutner <51681475+isabellekeutner@users.noreply.github.com>
2019-07-04 22:59:19 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,11 @@ from django.shortcuts import render, get_object_or_404
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'), 
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('event/', views.event_list, name='event_list'),
+    path('event/<int:pk>/', views.event_detail, name='event_detail'),
+    path('event/new/', views.event_new, name='event_new'),
+    path('event/<int:pk>/edit/', views.event_edit, name='event_edit'),
 ]
 
